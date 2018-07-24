c0b3dcf80b284a13dc31f39c15a1e5ed9f2de143
Allison <allisonkingwilliams@gmail.com>
2018-07-24 17:25:54 -0400

    captions and images on index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,6 +105,7 @@ $(document).ready(function() {
             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
+            verticalFit: true,
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
             titleSrc: function(item) {
                 return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.src  +'" </a>';
